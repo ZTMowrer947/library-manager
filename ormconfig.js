d020b5f8fe21731c5d81650d6638cca864555950
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-10 14:56:42 -0500

    Add TypeORM config file

---
+++ ormconfig.js
@@ -0,0 +1,59 @@
+// Imports
+const path = require("path");
+
+// Environment configuration
+const nodeEnv = process.env.NODE_ENV || "development";
+const env = (() => {
+    switch (nodeEnv) {
+        case "prod":
+        case "production":
+            return "production";
+
+        case "staging":
+        case "testing":
+        case "test":
+            return "testing";
+
+        default:
+            return "development";
+    }
+})();
+
+// Environment-dependent configuration options
+const basePath = path.resolve(
+    __dirname,
+    env === "production" ? "dist" : "src",
+    "database"
+);
+const dbFile =
+    env === "testing" ? ":memory" : path.resolve(basePath, "library.db");
+const migrationsRun = env === "production";
+const synchronize = !migrationsRun;
+
+// Configuration
+/**
+ * @type {import("typeorm").ConnectionOptions}
+ */
+const connectionOptions = {
+    // Use SQLite database
+    type: "sqlite",
+
+    // Database path
+    database: dbFile,
+
+    // Directory paths
+    entities: [path.resolve(basePath, "entities", "**", "*.ts")],
+    migrations: [path.resolve(basePath, "migrations", "**", "*.ts")],
+
+    cli: {
+        entitiesDir: path.resolve(basePath, "entities"),
+        migrationsDir: path.resolve(basePath, "migrations"),
+    },
+
+    // Database sync options
+    migrationsRun,
+    synchronize,
+};
+
+// Export
+module.exports = connectionOptions;
