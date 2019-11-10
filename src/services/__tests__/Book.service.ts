03e4e9f3736c72e090d15a7ae9fc9cd7db606c08
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-10 17:57:35 -0500

    Fill out getList and nil getById BookService tests

--- src/services/__tests__/Book.service.ts
+++ src/services/__tests__/Book.service.ts
@@ -1,10 +1,32 @@
 // Imports
+import base32 from "hi-base32";
+import { getRepository } from "typeorm";
 import BookService from "../Book.service";
+import Book from "../../database/entities/Book.entity";
 
 // Test Suite
 describe("BookService", () => {
+    let bookService: BookService;
+    let nilId: string;
+
+    // Run before each test
+    beforeAll(() => {
+        // Get book repository
+        const repository = getRepository(Book);
+
+        // Initialize book service
+        bookService = new BookService(repository);
+
+        // Set nil id
+        const nilBytes = Buffer.alloc(15);
+        nilId = base32.encode(nilBytes);
+    });
+
     describe(".getList", () => {
-        it.todo("should retrieve a list of books");
+        it("should retrieve a list of books", async () => {
+            // Expect book listing to have length 15
+            await expect(bookService.getList()).resolves.toHaveLength(15);
+        });
     });
 
     describe(".create", () => {
@@ -14,9 +36,10 @@ describe("BookService", () => {
     describe(".getById", () => {
         it.todo("should retrieve a single book by its id");
 
-        it.todo(
-            "should resolve to undefined if no book exists with the given id"
-        );
+        it("should resolve to undefined if no book exists with the given id", async () => {
+            // Expect book retrieval to result in undefined with nil id
+            await expect(bookService.getById(nilId)).resolves.toBeUndefined();
+        });
     });
 
     describe(".update", () => {
