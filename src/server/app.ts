4134988b431aece15bd77353930675f893f83712
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-10 21:43:35 -0500

    Add koa webpack middleware to app

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,6 +3,7 @@ import path from "path";
 import Koa from "koa";
 import views from "koa-views";
 import router from "./routes";
+import env, { EnvType } from "./env";
 
 // Paths
 const viewsPath = path.resolve(__dirname, "..", "..", "views");
@@ -10,16 +11,37 @@ const viewsPath = path.resolve(__dirname, "..", "..", "views");
 // App setup
 const app = new Koa();
 
-// Middleware
-app.use(
-    views(viewsPath, {
-        extension: "pug",
-    })
-);
+(async () => {
+    // Middleware
+    app.use(
+        views(viewsPath, {
+            extension: "pug",
+        })
+    );
 
-// Routes
-app.use(router.routes());
-app.use(router.allowedMethods());
+    // Asset setup
+
+    // If we are in development mode,
+    if (env === EnvType.Development) {
+        // Import webpack packages
+        const { default: koaWebpack } = await import("koa-webpack");
+        const { default: webpack } = await import("webpack");
+        const { default: webpackConfig } = await import("../../webpack.config");
+
+        // Webpack compiler setup
+        const compiler = webpack(webpackConfig);
+
+        // Setup middleware
+        const middleware = await koaWebpack({ compiler });
+
+        // Apply middleware
+        app.use(middleware);
+    }
+
+    // Routes
+    app.use(router.routes());
+    app.use(router.allowedMethods());
+})();
 
 // Export
 export default app;
