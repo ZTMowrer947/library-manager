927051374810860dd419c35de3f5e3ea142011af
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-12 16:52:11 -0500

    Add HTTP Server setup to test setup

--- src/server/testSetup.ts
+++ src/server/testSetup.ts
@@ -1,15 +1,29 @@
 // Imports
 import { getConnection } from "typeorm";
+import app from "./app";
 import ormBootstrap from "./database";
+import http from "http";
+
+// Variables
+let server: http.Server;
 
 // Run before all tests
 beforeAll(async () => {
     // Setup database connection
     await ormBootstrap();
+
+    // Initialize HTTP server
+    server = http.createServer(app.callback());
+
+    // Listen on port 53035
+    server.listen(53035);
 });
 
 // Run after all tests
 afterAll(async () => {
+    // Close HTTP server
+    server.close();
+
     // Get database connection
     const connection = getConnection();
 
