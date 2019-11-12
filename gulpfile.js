c19a5d1f2eeaeb17a9f24c95722871f25ccbeddd
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-12 15:03:15 -0500

    Adjust gulp task globs
    Adjust server build task to exclude test files
    
    Remove TypeScript server project from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,35 @@
 // Imports
-const path = require("path");
 const del = require("del");
 const gulp = require("gulp");
 const babel = require("gulp-babel");
 const sm = require("gulp-sourcemaps");
 const terser = require("gulp-terser");
-const ts = require("gulp-typescript");
-const merge = require("merge2");
 const webpack = require("webpack");
 const webpackStream = require("webpack-stream");
 const webpackConfig = require("./webpack.config");
 
-// TypeScript project setup
-const tsProject = ts.createProject(
-    path.resolve(__dirname, "src", "server", "tsconfig.json")
-);
-
 // Tasks
 const buildServer = () => {
-    const babelResult = tsProject
-        .src()
+    return gulp
+        .src([
+            "src/server/**/*.ts",
+            "!src/server/**/__tests__/**/*.ts",
+            "!src/server/testSetup.ts",
+        ])
         .pipe(sm.init())
         .pipe(babel())
         .pipe(terser())
-        .pipe(sm.write("."));
-
-    const tsResult = tsProject.src().pipe(tsProject());
-
-    return merge([
-        babelResult.pipe(gulp.dest(path.join("dist", "server"))),
-        tsResult.dts.pipe(gulp.dest(path.join("dist", "server"))),
-    ]);
+        .pipe(sm.write("."))
+        .pipe(gulp.dest("dist/server"));
 };
 
 const buildClient = () => {
     return gulp
-        .src(path.resolve(__dirname, "src", "client", "index.ts"))
+        .src("src/client/index.ts")
         .pipe(sm.init())
         .pipe(webpackStream(webpackConfig, webpack))
         .pipe(sm.write("."))
-        .pipe(gulp.dest(path.join("dist", "client")));
+        .pipe(gulp.dest("dist/client"));
 };
 
 const build = gulp.series(buildClient, buildServer);
