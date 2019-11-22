02a0265817da1fc827871d9095ec59d5dcd80f70
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-22 18:34:16 -0500

    Remove HTTP server setup from test setup

--- src/server/testSetup.ts
+++ src/server/testSetup.ts
@@ -1,12 +1,7 @@
 // Imports
 import { Container } from "typedi";
 import { getConnection, useContainer } from "typeorm";
-import app from "./app";
 import ormBootstrap from "./database";
-import http from "http";
-
-// Variables
-let server: http.Server;
 
 // Run before all tests
 beforeAll(async () => {
@@ -15,23 +10,10 @@ beforeAll(async () => {
 
     // Setup database connection
     await ormBootstrap();
-
-    // Initialize HTTP server
-    server = http.createServer(app.callback());
-
-    // Listen on port 53035
-    server.listen(53035);
 });
 
 // Run after all tests
 afterAll(async () => {
-    // Close HTTP server
-    const closeAsync = new Promise((resolve, reject) =>
-        server.close(err => (err ? reject(err) : resolve()))
-    );
-
-    await closeAsync;
-
     // Get database connection
     const connection = getConnection();
 
