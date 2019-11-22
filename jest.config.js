89ffbb69154ce1a63fdddc017877ee8bc7aed2d7
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-22 18:33:32 -0500

    Remove e2e files

--- jest.config.js
+++ jest.config.js
@@ -84,7 +84,7 @@ module.exports = {
     // notifyMode: "failure-change",
 
     // A preset that is used as a base for Jest's configuration
-    preset: "jest-puppeteer",
+    // preset: null,
 
     // Run tests from one or more projects
     // projects: null,
@@ -125,7 +125,7 @@ module.exports = {
     // snapshotSerializers: [],
 
     // The test environment that will be used for testing
-    // testEnvironment: "node",
+    testEnvironment: "node",
 
     // Options that will be passed to the testEnvironment
     // testEnvironmentOptions: {},
