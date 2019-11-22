7a04517ac3edcf8f9df1804459139f2867120a98
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-22 18:25:46 -0500

    Rewrite homepage e2e test with new preset

--- src/server/e2e/home.e2e.ts
+++ src/server/e2e/home.e2e.ts
@@ -1,48 +1,8 @@
 // Imports
 import cheerio from "cheerio";
-import puppeteer from "puppeteer";
 
 // Test Suite
 describe("Home page", () => {
-    let browser: puppeteer.Browser;
-    let page: puppeteer.Page;
-
-    // Run before all tests
-    beforeAll(async () => {
-        // Launch browser
-        browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 100,
-        });
-    });
-
-    // Run before each test
-    beforeEach(async () => {
-        // Open new page
-        page = await browser.newPage();
-
-        // Set viewport and user agent
-        page.emulate({
-            viewport: {
-                width: 800,
-                height: 600,
-            },
-            userAgent: "",
-        });
-    });
-
-    // Run after each test
-    afterEach(async () => {
-        // Close page
-        await page.close();
-    });
-
-    // Run after all tests
-    afterAll(async () => {
-        // Close browser
-        await browser.close();
-    });
-
     it("should load correctly", async () => {
         // Go to book listing
         await page.goto("http://localhost:53035");
