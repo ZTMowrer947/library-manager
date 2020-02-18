ccc56c270959fe128be9671ef8eb99e03b25ef6f
Zackery Mowrer <Zackmowrer@gmail.com>
2020-02-17 20:29:29 -0500

    Add bootstrap styles and NgbModule to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
     declarations: [AppComponent],
     imports: [
         BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        NgbModule,
         AppRoutingModule,
     ],
     providers: [],
