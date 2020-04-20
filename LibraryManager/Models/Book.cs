935aa9f5524e2a067db897246d6611bfbf240bc2
Zackery Mowrer <Zackmowrer@gmail.com>
2020-04-20 03:22:07 -0400

    Add Entity and Book classes

---
+++ LibraryManager/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LibraryManager.Models
+{
+	public class Book : Entity<ulong>, IEquatable<Book>
+	{
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public string Genre { get; set; }
+		public int Year { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as Book);
+		}
+
+		public bool Equals(Book other)
+		{
+			return other != null &&
+				   Title == other.Title &&
+				   Author == other.Author &&
+				   Genre == other.Genre &&
+				   Year == other.Year;
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Title, Author, Genre, Year);
+		}
+	}
+}
---
+++ LibraryManager/Models/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryManager.Models
+{
+	public abstract class Entity<TIdType>
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public TIdType Id { get; set; }
+	}
+}
