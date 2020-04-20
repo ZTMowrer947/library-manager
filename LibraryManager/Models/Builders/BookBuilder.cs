dc385a4eb241fee473a406863b589c5b0fb8be25
Zackery Mowrer <Zackmowrer@gmail.com>
2020-04-20 03:32:58 -0400

    Add BookBuilder class

---
+++ LibraryManager/Models/Builders/BookBuilder.cs
@@ -0,0 +1,49 @@
+﻿namespace LibraryManager.Models.Builders
+{
+	public class BookBuilder
+	{
+		private string _title;
+		private string _author;
+		private string _genre;
+		private int _year;
+
+		public BookBuilder()
+		{
+		}
+
+		public BookBuilder WithTitle(string title)
+		{
+			_title = title;
+			return this;
+		}
+
+		public BookBuilder WithAuthor(string author)
+		{
+			_author = author;
+			return this;
+		}
+
+		public BookBuilder WithGenre(string genre)
+		{
+			_genre = genre;
+			return this;
+		}
+
+		public BookBuilder WithYear(int year)
+		{
+			_year = year;
+			return this;
+		}
+
+		public Book Build()
+		{
+			return new Book
+			{
+				Title = _title,
+				Author = _author,
+				Genre = _genre,
+				Year = _year
+			};
+		}
+	}
+}
