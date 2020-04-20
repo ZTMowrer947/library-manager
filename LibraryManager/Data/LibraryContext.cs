9a740382e42b8bbeac031d94331c3a58cfcb473e
Zackery Mowrer <Zackmowrer@gmail.com>
2020-04-20 04:05:05 -0400

    Add LibraryContext with DI configuration

---
+++ LibraryManager/Data/LibraryContext.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManager.Data
+{
+	public class LibraryContext : DbContext
+	{
+		public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Book> Books { get; set; }
+	}
+}
--- LibraryManager/Startup.cs
+++ LibraryManager/Startup.cs
@@ -1,7 +1,9 @@
+using LibraryManager.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +22,13 @@ namespace LibraryManager
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			// Register database context
+			services.AddDbContextPool<LibraryContext>(options =>
+			{
+				// Use in-memory database
+				options.UseInMemoryDatabase("library");
+			});
+
 			services.AddControllersWithViews();
 			// In production, the Angular files will be served from this directory
 			services.AddSpaStaticFiles(configuration =>
@@ -31,6 +40,16 @@ namespace LibraryManager
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			// Create service scope
+			using (var scope = app.ApplicationServices.CreateScope())
+			{
+				// Get database context
+				var context = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+
+				// Ensure database is created
+				context.Database.EnsureCreated();
+			}
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
