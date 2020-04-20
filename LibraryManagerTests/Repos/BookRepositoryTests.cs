1c4bf957279ebdfa3f330ced56c0277f06b569cd
Zackery Mowrer <Zackmowrer@gmail.com>
2020-04-20 11:04:11 -0400

    Fill out tests for Book repository

--- LibraryManagerTests/Repos/BookRepositoryTests.cs
+++ LibraryManagerTests/Repos/BookRepositoryTests.cs
@@ -3,39 +3,146 @@ using LibraryManager.Repos;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using LibraryManager.Data;
+using Microsoft.EntityFrameworkCore;
+using LibraryManager.Models;
+using LibraryManagerTests.Utils;
+using System.Linq;
 
 namespace LibraryManager.Repos.Tests
 {
-	public class BookRepositoryTests
+	public class LibraryContextFixture : IDisposable
 	{
-		[Fact()]
-		public void FindAllTest()
+		public LibraryContext Context { get; private set; }
+		public IRepository<Book, ulong> Repository { get; private set; }
+		public ulong TestBookId => Context.Books.AsNoTracking().Last().Id;
+
+		public LibraryContextFixture()
 		{
-			Assert.True(false, "This test needs an implementation");
+			// Configure context
+			var options = new DbContextOptionsBuilder<LibraryContext>()
+				.UseInMemoryDatabase(databaseName: "library-book-testing")
+				.Options;
+
+			// Initialize and setup context
+			Context = new LibraryContext(options);
+			Context.Database.EnsureCreated();
+
+			// Seed database with test data
+			Context.EnsureSeeded();
+
+			// Initialize repository under test
+			Repository = new BookRepository(Context);
+		}
+
+		public void Dispose()
+		{
+			// Dispose context
+			Context.Dispose();
+		}
+	}
+
+	public class BookRepositoryTests : IClassFixture<LibraryContextFixture>
+	{
+		private readonly LibraryContextFixture _fixture;
+
+		public BookRepositoryTests(LibraryContextFixture fixture)
+		{
+			_fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
 		}
 
 		[Fact()]
-		public void FindByIdTest()
+		public void FindAll_ShouldReturnNonEmptyBookListing()
 		{
-			Assert.True(false, "This test needs an implementation");
+			// Get listing from repository
+			var books = _fixture.Repository.FindAll();
+
+			// Assert that collection has nonzero size
+			Assert.True(books.Count > 0, "FindAll method should return non-empty book listing");
 		}
 
 		[Fact()]
-		public void CreateTest()
+		public void FindById_ShouldReturnNullIfCourseIsNotFound()
 		{
-			Assert.True(false, "This test needs an implementation");
+			// Define ID of nonexistent book
+			var id = ulong.MaxValue;
+
+			// Attempt to get book from repository
+			var book = _fixture.Repository.FindById(id);
+
+			// Assert that book is null
+			Assert.Null(book);
 		}
 
 		[Fact()]
-		public void UpdateTest()
+		public void Create_ShouldPersistBook()
 		{
-			Assert.True(false, "This test needs an implementation");
+			// Generate fake book data
+			var newBook = BookUtils.GetFakeBook();
+
+			// Save book using repository
+			_fixture.Repository.Create(newBook);
+
+			// Retrieve new book using context
+			var retrievedBook = _fixture.Context.Books
+				.Where(book => book.Id == newBook.Id)
+				.AsNoTracking()
+				.SingleOrDefault();
+
+			// Assert that retrieved book is non-null and matches created book
+			Assert.NotNull(retrievedBook);
+			Assert.StrictEqual(newBook, retrievedBook);
 		}
 
 		[Fact()]
-		public void DeleteTest()
+		public void Update_ShouldModifyExistingBook()
 		{
-			Assert.True(false, "This test needs an implementation");
+			// Get test ID
+			var testId = _fixture.TestBookId;
+
+			// Retrieve current book with ID using context
+			var currentBook = _fixture.Context.Books
+				.Where(book => book.Id == testId)
+				.AsNoTracking()
+				.SingleOrDefault();
+
+			// Generate fake book update data
+			var updatedBook = BookUtils.GetFakeBook();
+
+			// Attach test book Id to book update data
+			updatedBook.Id = testId;
+
+			// Update book using repository
+			_fixture.Repository.Update(updatedBook);
+
+			// Assert that books share the same ID, but are not equal
+			Assert.StrictEqual(currentBook.Id, updatedBook.Id);
+			Assert.NotStrictEqual(currentBook, updatedBook);
+		}
+
+		[Fact()]
+		public void Delete_ShouldRemoveBook()
+		{
+			// Get test ID
+			var testId = _fixture.TestBookId;
+
+			// Retrieve book to delete with ID using context
+			var bookToDelete = _fixture.Context.Books
+				.Where(book => book.Id == testId)
+				.AsNoTracking()
+				.SingleOrDefault();
+
+			// Delete book using repository
+			_fixture.Repository.Delete(bookToDelete);
+
+			// Attempt to fetch book again using context
+			var deletedBook = _fixture.Context.Books
+				.Where(book => book.Id == testId)
+				.AsNoTracking()
+				.SingleOrDefault();
+
+			// Assert that post-delete fetch returned null
+			Assert.Null(deletedBook);
 		}
 	}
 }
---
+++ LibraryManagerTests/Utils/BookUtils.cs
@@ -0,0 +1,27 @@
+﻿using LibraryManager.Models;
+using LibraryManager.Models.Builders;
+using System;
+
+namespace LibraryManagerTests.Utils
+{
+	class BookUtils
+	{
+		public static Book GetFakeBook()
+		{
+			// Generate properties of new book
+			var title = string.Join(" ", Faker.Lorem.Words(3));
+			var author = Faker.Name.FullName();
+			var genre = "Testing";
+			var currentYear = new DateTime().Year;
+			var bookYear = currentYear - Faker.RandomNumber.Next(1, 50);
+
+			// Build and return book
+			return new BookBuilder()
+				.WithTitle(title)
+				.WithAuthor(author)
+				.WithGenre(genre)
+				.WithYear(bookYear)
+				.Build();
+		}
+	}
+}
