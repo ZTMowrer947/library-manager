0d9ec6705a0ce25fad411221f7d5e0fba4a22196
Zackery Mowrer <Zackmowrer@gmail.com>
2020-04-20 09:26:11 -0400

    Add test project with stubbed out BookRepository tests

---
+++ LibraryManagerTests/Repos/BookRepositoryTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using LibraryManager.Repos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.Repos.Tests
+{
+	public class BookRepositoryTests
+	{
+		[Fact()]
+		public void FindAllTest()
+		{
+			Assert.True(false, "This test needs an implementation");
+		}
+
+		[Fact()]
+		public void FindByIdTest()
+		{
+			Assert.True(false, "This test needs an implementation");
+		}
+
+		[Fact()]
+		public void CreateTest()
+		{
+			Assert.True(false, "This test needs an implementation");
+		}
+
+		[Fact()]
+		public void UpdateTest()
+		{
+			Assert.True(false, "This test needs an implementation");
+		}
+
+		[Fact()]
+		public void DeleteTest()
+		{
+			Assert.True(false, "This test needs an implementation");
+		}
+	}
+}
