216438254e2aabf903d34981961b0daf578837d7
Zackery Mowrer <Zackmowrer@gmail.com>
2020-04-20 13:33:40 -0400

    Stub out tests for BookService

---
+++ LibraryManagerTests/Services/BookServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using LibraryManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager.Services.Tests
+{
+	public class BookServiceTests
+	{
+		[Fact()]
+		public async Task GetListTest()
+		{
+			Assert.True(false, "This test needs an implementation");
+		}
+
+		[Fact()]
+		public async Task CreateTest()
+		{
+			Assert.True(false, "This test needs an implementation");
+		}
+
+		[Fact()]
+		public async Task GetByIdTest()
+		{
+			Assert.True(false, "This test needs an implementation");
+		}
+
+		[Fact()]
+		public async Task UpdateTest()
+		{
+			Assert.True(false, "This test needs an implementation");
+		}
+
+		[Fact()]
+		public async Task DeleteTest()
+		{
+			Assert.True(false, "This test needs an implementation");
+		}
+	}
+}
