605a14fb3ff88321a2c85f0eeb7673b8196a61ef
Zackery Mowrer <Zackmowrer@gmail.com>
2020-04-21 15:52:12 -0400

    Stub out Book controller

---
+++ LibraryManager/Controllers/BooksController.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+using LibraryManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LibraryManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ICollection<Book>> Get()
+        {
+            return new List<Book>();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Book bookData)
+        {
+            return StatusCode(StatusCodes.Status503ServiceUnavailable);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(ulong id)
+        {
+            return StatusCode(StatusCodes.Status503ServiceUnavailable);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(ulong id, [FromBody] Book updatedData)
+        {
+            return StatusCode(StatusCodes.Status503ServiceUnavailable);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(ulong id)
+        {
+            return StatusCode(StatusCodes.Status503ServiceUnavailable);
+        }
+    }
+}
