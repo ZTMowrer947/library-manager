d3e8fb756667a350fd84bfe59be58d80bd1b9251
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-13 21:19:02 -0400

    Add IPagedRepository and PagedEntityRepository

---
+++ LibraryManager/Repos/PagedEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using LibraryManager.Data;
+using LibraryManager.Models;
+using System.Threading.Tasks;
+
+namespace LibraryManager.Repos
+{
+	public abstract class PagedEntityRepository<TEntity, TIdType> : EntityRepository<TEntity, TIdType>, IPagedRepository<TEntity, TIdType>
+		where TEntity : Entity<TIdType>
+	{
+		protected PagedEntityRepository(LibraryContext context) : base(context)
+		{
+		}
+
+		public abstract Task<Page<TEntity>> FindPage(int pageNumber, int itemsPerPage);
+	}
+}
