b0c990968bc5ab58eec4b7c6fb8163ee7ef307a0
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-14 06:21:17 -0400

    Add IPagedService and PagedEntityService

---
+++ LibraryManager/Services/PagedEntityService.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManager.Models;
+using LibraryManager.Repos;
+using System.Threading.Tasks;
+
+namespace LibraryManager.Services
+{
+	public class PagedEntityService<TEntity, TIdType> : EntityService<TEntity, TIdType>, IPagedService<TEntity, TIdType>
+		where TEntity : Entity<TIdType>
+	{
+		protected new IPagedRepository<TEntity, TIdType> _repository;
+
+		public PagedEntityService(IPagedRepository<TEntity, TIdType> repository) : base(repository)
+		{
+			_repository = repository;
+		}
+
+		public Task<Page<TEntity>> GetPage(int pageNumber, int itemsPerPage)
+		{
+			return _repository.FindPage(pageNumber, itemsPerPage);
+		}
+	}
+}
