e1fcdec42670c9b7e82e3673a67a678f21fb5c84
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-13 20:41:49 -0400

    Add Page model

---
+++ LibraryManager/Models/Page.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LibraryManager.Models
+{
+	public class Page<T> : IReadOnlyCollection<T>
+	{
+		public int PageNumber { get; }
+		public int TotalPages { get; }
+		public int ItemsPerPage { get; }
+		public ICollection<T> Data { get; }
+
+		public int Count => Data.Count;
+
+		public Page(int pageNumber, int totalPages, int itemsPerPage, ICollection<T> data)
+		{
+			PageNumber = pageNumber;
+			TotalPages = totalPages;
+			ItemsPerPage = itemsPerPage;
+			Data = data ?? throw new ArgumentNullException(nameof(data));
+		}
+
+		public static Page<T> FromCollection(ICollection<T> data)
+		{
+			return new Page<T>(pageNumber: 1, totalPages: 1, itemsPerPage: data.Count, data);
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			return Data.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
