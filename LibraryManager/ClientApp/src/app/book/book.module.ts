255b4f84b3941bc223e371177e30f8eb4b07d350
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-16 12:55:34 -0400

    Attach BookService to BookModule

--- LibraryManager/ClientApp/src/app/book/book.module.ts
+++ LibraryManager/ClientApp/src/app/book/book.module.ts
@@ -2,9 +2,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BookService } from './book.service';
+
 // Module
 @NgModule({
     declarations: [],
     imports: [CommonModule],
+    providers: [BookService],
 })
 export class BookModule {}
