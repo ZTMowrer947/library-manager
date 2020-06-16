cb422f421f68efb6574b6cfe3c21c02199ee9c7e
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-16 11:41:43 -0400

    Add book module

---
+++ LibraryManager/ClientApp/src/app/book/book.module.ts
@@ -0,0 +1,10 @@
+// Imports
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+// Module
+@NgModule({
+    declarations: [],
+    imports: [CommonModule],
+})
+export class BookModule {}
