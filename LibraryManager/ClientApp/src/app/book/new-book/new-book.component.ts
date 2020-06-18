ca10de15e0e8a2e176b24a6848b6e0f44f0e53b3
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-18 14:28:32 -0400

    Update new-book component to actually create new book

--- LibraryManager/ClientApp/src/app/book/new-book/new-book.component.ts
+++ LibraryManager/ClientApp/src/app/book/new-book/new-book.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
 import { BookCreateViewmodel } from '../book-create-viewmodel';
+import { BookService } from '../book.service';
 
 @Component({
     selector: 'app-new-book',
@@ -10,7 +13,11 @@ import { BookCreateViewmodel } from '../book-create-viewmodel';
 export class NewBookComponent implements OnInit {
     public newBookForm: FormGroup;
 
-    public constructor(private fb: FormBuilder) {
+    public constructor(
+        private router: Router,
+        private fb: FormBuilder,
+        private bookService: BookService
+    ) {
         this.newBookForm = this.fb.group({
             title: [''],
             author: [''],
@@ -35,10 +42,15 @@ export class NewBookComponent implements OnInit {
         const viewModel: BookCreateViewmodel = {
             title: formData.title.trim(),
             author: formData.author.trim(),
-            genre: formData.author.trim() || undefined,
+            genre: formData.genre.trim() || undefined,
             year: normalizedYear,
         };
 
-        console.log(viewModel);
+        this.bookService.create(viewModel).subscribe({
+            complete: () => {
+                // When complete, redirect to home page
+                this.router.navigate(['books']);
+            },
+        });
     }
 }
