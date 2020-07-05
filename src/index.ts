faf071a6aa28dff65e001cc0c8dd96f60f908509
Zackery Mowrer <Zackmowrer@gmail.com>
2020-07-04 23:00:42 -0400

    Add project files from ts-babel-starter

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// Imports
+import add from './add';
+
+// Define addends and compute sum
+const num1 = 5;
+const num2 = 37;
+const sum = add(num1, num2);
+
+// Print out result
+console.log(`${num1} + ${num2} = ${sum}`); // eslint-disable-line no-console
