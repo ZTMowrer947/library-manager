c3e1b45dcfca79b2037eb95facde7d58476e57c9
Zackery Mowrer <Zackmowrer@gmail.com>
2020-07-07 19:20:26 -0400

    Refactor book list route tests' server setup

--- src/pages/api/books/__tests__/index.ts
+++ src/pages/api/books/__tests__/index.ts
@@ -8,18 +8,33 @@ import BookDto from '@/dto/BookDto';
 import handler from '@/pages/api/books';
 import BookService from '@/services/BookService';
 
-// Jest Mocks
+// Mock Setup
 jest.mock('@/services/BookService');
 jest.mock('@/middleware/withDatabaseConnection');
 
 // Test Suite
 describe('Book listing routes', () => {
     let service: BookService;
+    let server: http.Server;
 
     beforeAll(() => {
         // Initailize book service
         // * This service is being mocked above, therefore connection object is not required
         service = new BookService(undefined as never);
+
+        // Create HTTP server from API handler
+        server = http.createServer(async (req, res) => {
+            return apiResolver(req, res, undefined, handler, {
+                previewModeId: '',
+                previewModeEncryptionKey: '',
+                previewModeSigningKey: '',
+            });
+        });
+    });
+
+    afterAll((done) => {
+        // Close server
+        return server.close(done);
     });
 
     describe('GET /api/books', () => {
@@ -31,45 +46,21 @@ describe('Book listing routes', () => {
                 return bookDto;
             });
 
-            // Create HTTP server from API handler
-            const server = http.createServer(async (req, res) => {
-                return apiResolver(req, res, undefined, handler, {
-                    previewModeId: '',
-                    previewModeEncryptionKey: '',
-                    previewModeSigningKey: '',
-                });
-            });
-
             // Make API response, expecting a 200 response
             const response = await request(server).get('/api/books').expect(200);
 
             // Expect response body to match expected book listing
             expect(response.body).toStrictEqual(expected);
-
-            // Close HTTP server
-            return server.close();
         });
     });
 
     describe('POST /api/books', () => {
         it('should respond with a 400 error if the request body is invalid', async () => {
-            // Create HTTP server from API handler
-            const server = http.createServer(async (req, res) => {
-                return apiResolver(req, res, undefined, handler, {
-                    previewModeId: '',
-                    previewModeEncryptionKey: '',
-                    previewModeSigningKey: '',
-                });
-            });
-
             // Make API response with empty body, expecting a 400 response
             const response = await request(server).post('/api/books').send({}).expect(400);
 
             // Expect response body to contain errors property
             expect(response.body).toHaveProperty('errors');
-
-            // Close HTTP server
-            return server.close();
         });
 
         it('should respond with a 201 status if the book was successfully created', async () => {
@@ -80,15 +71,6 @@ describe('Book listing routes', () => {
                 genre: 'Informational',
                 year: 2020,
             };
-
-            // Create HTTP server from API handler
-            const server = http.createServer(async (req, res) => {
-                return apiResolver(req, res, undefined, handler, {
-                    previewModeId: '',
-                    previewModeEncryptionKey: '',
-                    previewModeSigningKey: '',
-                });
-            });
 
             // Make API response with book data as request body, expecting a 201 response
             const response = await request(server).post('/api/books').send(bookData).expect(201);
@@ -103,29 +85,14 @@ describe('Book listing routes', () => {
 
             // Expect location header be set as expected
             expect(response.header).toHaveProperty('location', expectedLocation);
-
-            // Close HTTP server
-            return server.close();
         });
     });
 
     it('should respond with a 405 error when trying to use an unsupported method', async () => {
-        // Create HTTP server from API handler
-        const server = http.createServer(async (req, res) => {
-            return apiResolver(req, res, undefined, handler, {
-                previewModeId: '',
-                previewModeEncryptionKey: '',
-                previewModeSigningKey: '',
-            });
-        });
-
         // Make API request with invalid method, expecting a 405 response
         const response = await request(server).propfind('/api/books').expect(405);
 
         // Expect endpoint to only allow GET and POST request
         expect(response.header).toHaveProperty('allow', 'GET, POST');
-
-        // Close HTTP server
-        return server.close();
     });
 });
