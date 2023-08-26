b1c408fbe2dc3b3bfbfd30f775e5c182482b35ba
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-08-26 16:41:52 -0400

    Add and setup basic TypeScript project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { createServer } from 'node:http';
+
+const server = createServer((_req, res) => {
+    res.end('Hello, world!')
+})
+
+server.listen(8080, () => {
+    console.log('Server now listening on port 8080');
+});
